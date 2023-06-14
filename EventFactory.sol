077fc5f3074ec31bffc7ef14c1171e2724e6ce36
Soumyadeep <soumyadeeproy.csbs@gmail.com>
2023-06-14 13:29:06 +0530

    Eventfactory is in working condition

--- EventFactory.sol
+++ EventFactory.sol
@@ -7,74 +7,57 @@ import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 pragma solidity ^0.8.0;
 
 contract EventFactory is ERC721URIStorage, EIP712 {
-    constructor(
-        address payable minter
-    ) ERC721("EventNFT", "ENT") EIP712("Event-NFT", "1") {}
+    string private constant SIGNING_DOMAIN = "Event-Domain";
+    string private constant SIGNATURE_VERSION = "1";
+
+    constructor() ERC721("EventNFT", "ENT") EIP712("Event-NFT", "1") {}
 
     using Counters for Counters.Counter;
     Counters.Counter private _eventIds;
     using ECDSA for bytes32;
-    struct Event {
+    struct EventVoucher {
         uint256 eventId;
         address creator;
         uint256 minPrice;
         string uri;
+        bytes signature;
     }
-    mapping(uint => Event) idToEvent;
+    mapping(uint => EventVoucher) idToEvent;
     event createdEvent(
         uint indexed eventId,
         address indexed creator,
         uint minPrice
     );
-    Event newEvent;
-
-    function createEvent(string memory _tokenURI, uint _minPrice) internal {
-        _eventIds.increment();
-        //    newEvent.eventId = _eventIds.current();
-        //     newEvent.minPrice = _minPrice;
-        //     newEvent.creator = msg.sender;
-        //     newEvent.uri=_tokenURI;
-        //     emit createdEvent (
-        //         newEvent.eventId,
-        //         msg.sender,
-        //         newEvent.minPrice
-
-        //     );
-        //     idToEvent[newEvent.eventId] = Event(
-        //         newEvent.eventId,
-        //         msg.sender,
-        //         newEvent.minPrice,
-        //         newEvent.uri
-
-        //     );
-    }
 
     function redeem(
         address redeemer,
-        Event calldata voucher,
+        EventVoucher calldata voucher,
         bytes memory signature
-    ) public payable returns (uint256) {
+    ) public payable {
         address signer = _verify(voucher, signature);
         require(msg.value >= voucher.minPrice, "Insufficient funds to redeem");
         _mint(signer, voucher.eventId);
         _setTokenURI(voucher.eventId, voucher.uri);
+        _transfer(signer, redeemer, voucher.eventId);
     }
 
     function _verify(
-        Event calldata voucher,
+        EventVoucher calldata voucher,
         bytes memory signature
     ) internal view returns (address) {
         bytes32 digest = _hash(voucher);
         return digest.toEthSignedMessageHash().recover(signature);
     }
 
-    function _hash(Event calldata voucher) internal view returns (bytes32) {
+    function _hash(
+        EventVoucher calldata voucher
+    ) internal view returns (bytes32) {
         return
             _hashTypedDataV4(
                 keccak256(
                     abi.encode(
                         keccak256(
-                            "Event(uint256 eventId, address creator,uint256 minPrice,string uri)"
+                            "Event(uint256 eventId,address creator,uint256 minPrice,string uri)"
                         ),
                         voucher.eventId,
                         voucher.creator,
